2bef6e31271a5f7377655f285dea52db54eae2e9
Alessandro Bonomo <75626033+AlessandroBonomo28@users.noreply.github.com>
2023-12-07 23:17:53 +0100

    pt2!!!

--- day 7/pt2.py
+++ day 7/pt2.py
@@ -6,7 +6,7 @@ KTJJT 220
 QQQJA 483
 """
 
-def occourrences(array):
+def occourences_and_pairs(array):
     occ = {}
     pairs = 0
     for i in set(array):
@@ -16,12 +16,6 @@ def occourrences(array):
             pairs += 1
     return occ,pairs
 
-def get_max(array):
-    max = 0
-    for i in array:
-        if i > max:
-            max = i
-    return max
 
 def calculate_power_with_jollys(power,jollys):
     if jollys ==0:
@@ -31,36 +25,34 @@ def calculate_power_with_jollys(power,jollys):
     elif power == 2:
         return calculate_power_with_jollys(4,jollys-1)
     elif power == 3:
-        return calculate_power_with_jollys(6,jollys-1)
+        return calculate_power_with_jollys(5,jollys-1)
     elif power == 4:
         return calculate_power_with_jollys(6,jollys-1)
     elif power == 5:
+        return calculate_power_with_jollys(6,jollys-1)
+    elif power == 6:
         return calculate_power_with_jollys(7,jollys-1)
     else:
-        return power
+        return 7
+
 def parse_match(match):
     hand, bid = match.split(" ")
     jollys = hand.count("J")
     hand_without_jolly = hand.replace("J","")
-    occourrences_hand,pairs = occourrences(hand_without_jolly)
-
-    is_high_card_without_jolly = True if len(occourrences_hand) == len(hand_without_jolly) else False
-
-    if jollys >0:
-        occourrences_hand["J"] = jollys
+    occourrences_hand,pairs = occourences_and_pairs(hand_without_jolly)
 
     length = len(occourrences_hand) 
-    if length == 5 and jollys == 0:
+    if length == len(hand_without_jolly):
         power = 1 # high card
-    elif is_high_card_without_jolly:
-        power = 1
-    elif length == 1:
+    elif length == 1 and jollys == 0:
+        power = 7 # five of a kind
+    elif jollys == 5:
         power = 7 # five of a kind
     else:
         if 4 in occourrences_hand.values():
-            power = 6 # full house
+            power = 6 # four of a kind
         elif 3 in occourrences_hand.values() and pairs == 1:
-            power = 5
+            power = 5 # full house
         elif 3 in occourrences_hand.values():
             power = 4 # three of a kind
         elif pairs == 1:
