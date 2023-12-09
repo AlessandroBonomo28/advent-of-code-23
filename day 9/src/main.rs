8abd1440cd2853f3b6d5758e9be7518ba7d8fa2e
Alexbonny28 <al.bonomo@studenti.unina.it>
2023-12-09 16:43:34 +0100

    idk

--- day 9/src/main.rs
+++ day 9/src/main.rs
@@ -6,7 +6,7 @@
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
-
+use std::env;
 fn create_sub_sequence(main_seq :&[i32]) -> Vec<i32> {
     let mut sub_sequence: Vec<i32> = Vec::new();
     let mut i = 1;
@@ -15,6 +15,7 @@ fn create_sub_sequence(main_seq :&[i32]) -> Vec<i32> {
         sub_sequence.push(diff);
         i += 1;
     }
+
     sub_sequence
 }
 
@@ -23,8 +24,23 @@ fn is_zero_sequence(sequence: &[i32]) -> bool {
     sum == 0
 }
 
+fn predict_next_value(sequences: &[Vec<i32>]) -> i32 {
+    let mut sum = 0;
+    for i in (0..sequences.len()).rev() {
+        sum += sequences[i][sequences[i].len()-1];
+    }
+    sum
+}
+
 fn main() {
-    if let Ok(lines) = read_lines("./calibration.txt") {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        println!("Please provide a filename");
+        return;
+    }
+    let filename : &String = &args[1];
+    let mut total = 0;
+    if let Ok(lines) = read_lines(filename) {
         for line in lines {
             if let Ok(values_str) = line {
                 let values : Vec<&str> = values_str.split(" ").collect();
@@ -34,18 +50,27 @@ fn main() {
                     .map(|&s| s.parse().expect("Failed to parse integer"))
                     .collect();
                 
-                let sub_sequence = create_sub_sequence(&sequence);
-                let sub_sequence2 = create_sub_sequence(&sub_sequence);
-                println!("Main sequence:");
-                dbg!(sequence); // print debug info
-                println!("Subsequence:");
-                dbg!(sub_sequence);
-                let res : bool = is_zero_sequence(&sub_sequence2);
-                println!("is zero: {}", res);
-                println!();
+                let mut sequences : Vec<Vec<i32>> = vec![sequence.clone()];
+                
+                for i in 1..sequence.len() {
+                    let sub_sequence = create_sub_sequence(&sequences[i-1]);
+                    let is_zero : bool = is_zero_sequence(&sub_sequence);
+                    if is_zero {
+                        sequences.push(sub_sequence.clone());
+                        break;
+                    } else {
+                        sequences.push(sub_sequence.clone());
+                    }
+                }
+
+                let prediction : i32 = predict_next_value(&sequences);
+                total += prediction;
+                //println!("Predict: {}", prediction);
+                //dbg!(sequences);
             }
         }
     }
+    println!("Total: {}", total);
 }
 
 // The output is wrapped in a Result to allow matching on errors
